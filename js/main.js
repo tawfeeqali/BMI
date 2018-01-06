a8f625d33a54d03dd1ac93906f2e5c7f14e8f59c
Tawfeeq Ali <Tawfeeq@Tawfeeqs-MBP.lan>
2018-01-06 18:05:00 +1100

    changes 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
+/*requirejs(["node_modules/bmi-calc/index.js"], function(calc) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util"
+	alert(calc(154,88,true));
+});*/
 
+var app = angular.module('myApp', []);
+app.controller('validateCtrl', function($scope) {
+	
+});
+
+function computeBMI()
+    {
+        //Obtain user inputs
+        var height=Number(document.getElementById("height").value);
+        var heightunits=document.getElementById("heightunits").value;
+        var weight=Number(document.getElementById("weight").value);
+        var weightunits=document.getElementById("weightunits").value;
+
+
+        //Convert all units to metric
+        if (heightunits=="inches") height/=39.3700787;
+        if (weightunits=="lb") weight/=2.20462;
+
+        //Perform calculation
+        var BMI=weight/Math.pow(height,2);
+
+        //Display result of calculation
+        document.getElementById("output").innerText=Math.round(BMI*100)/100;
+
+        var output =  Math.round(BMI*100)/100;
+        if (output<18.5)
+        document.getElementById("comment").innerText = "Underweight";
+      else   if (output>=18.5 && output<=25)
+        document.getElementById("comment").innerText = "Normal";
+     else   if (output>=25 && output<=30)
+        document.getElementById("comment").innerText = "Obese";
+     else   if (output>30)
+        document.getElementById("comment").innerText = "Overweight";
+       // document.getElementById("answer").value = output; 
+    }
