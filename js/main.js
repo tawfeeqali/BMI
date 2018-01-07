e0b80e3dcdb0be181e0e25b9d1b05f14a25d9e4d
Tawfeeq Ali <Tawfeeq@Tawfeeqs-MacBook-Pro.local>
2018-01-07 17:18:47 +1100

    ui fixes

--- js/main.js
+++ js/main.js
@@ -1,31 +1,19 @@
-/*requirejs(["node_modules/bmi-calc/index.js"], function(calc) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util"
-	alert(calc(154,88,true));
-});*/
-var options = {}
-var app = angular.module('myApp', []);
-app.controller('validateCtrl', function($scope) {
-	$scope.myheight = 0.0;
-	$scope.myweight = 0.0;
-});
 
-
-$( "#calcBMI" ).on( "click", function() {
-      $( "#Results" ).show( "blind", options, 500 );
-});
+$("#calcForm").validate({
+  submitHandler: function(form) {
+    	computeBMI();
+	  	var options = {};
+      	$( "#Results" ).show("blind", options, 500);
+  }
+ });
 
 function computeBMI()
-    {
-		
+    {		
         //Obtain user inputs
-        var height=Number(document.getElementById("height").value);
-        var heightunits=document.getElementById("heightunits").value;
-        var weight=Number(document.getElementById("weight").value);
-        var weightunits=document.getElementById("weightunits").value;
-
+        var height=Number($("#height").val());
+        var heightunits=$("heightunits").val();
+        var weight=Number($("#weight").val());
+        var weightunits=$("#weightunits").val();
 
         //Convert all units to metric
         if (heightunits=="inches") height/=39.3700787;
@@ -35,16 +23,14 @@ function computeBMI()
         var BMI=weight/Math.pow(height,2);
 
         //Display result of calculation
-        document.getElementById("output").innerText=Math.round(BMI*100)/100;
-
         var output =  Math.round(BMI*100)/100;
+		$("#output").text(output);
         if (output<18.5)
-        document.getElementById("comment").innerText = "Underweight";
-      else   if (output>=18.5 && output<=25)
-        document.getElementById("comment").innerText = "Normal";
-     else   if (output>=25 && output<=30)
-        document.getElementById("comment").innerText = "Obese";
-     else   if (output>30)
-        document.getElementById("comment").innerText = "Overweight";
-       // document.getElementById("answer").value = output; 
+        $("#comment").text("Underweight");
+      	else   if (output>=18.5 && output<=25)
+        $("#comment").text("Normal");
+     	else   if (output>=25 && output<=30)
+        $("#comment").text("Obese");
+     	else   if (output>30)
+        $("#comment").text("Overweight");
     }
